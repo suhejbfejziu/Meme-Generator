f33f7077c45e295b3b657f776858408c4ad0f36b
Suhejb <fejziusuhejb12@gmail.com>
2023-02-16 22:07:27 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <img 
+            src="/assets/troll-face.png"
+            className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React - Project 3</h4>
+        </header>
+    )
+}
